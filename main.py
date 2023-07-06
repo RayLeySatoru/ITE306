fe3ccfda131feb05d3391fd4c6bc52311a6ef646
RayLeySatoru <127216478+RayLeySatoru@users.noreply.github.com>
2023-07-06 13:57:02 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+print("Programmer: Railey Murillo")
+print("Date Created: July 6, 2023")
+print("Description of the Program")
+print("First Name: Railey")
+print("Middle Name: Retotar")
+print("Last Name: Murillo")
+print("Course: BSIT")
+print("Year: 3RD YEAR")
+print("Age: 20")
+print("Gender: Male")
+print("Civil Status: Single")
+print("Nationality: Filipino")
+print("Motto: When life gives you lemons, make lemonade.")
+
+print("School: PHINMA Araullo University")
+print("College: CITE")
+
+
+num1 =  5
+num2 = 8
+
+sum =  num1 + num2
+print("SUM = {0}".format(sum))
+
+difference =  num1 - num2
+print("difference = {0}".format(difference))
+
+product =  num1 * num2
+print("product = {0}".format(product))
+
+quotient =  num1 / num2
+print("quotient = {0}".format(quotient))
