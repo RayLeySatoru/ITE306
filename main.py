499ac641298859e30a8c0538598bed1160d95e01
RayLeySatoru <127216478+RayLeySatoru@users.noreply.github.com>
2023-07-20 13:49:48 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,17 @@
-print("Programmer: Railey Murillo")
-print("Date Created: July 6, 2023")
-print("Description of the Program")
-print("First Name: Railey")
-print("Middle Name: Retotar")
-print("Last Name: Murillo")
-print("Course: BSIT")
-print("Year: 3RD YEAR")
-print("Age: 20")
-print("Gender: Male")
-print("Civil Status: Single")
-print("Nationality: Filipino")
-print("Motto: When life gives you lemons, make lemonade.")
+meter =  float(input('Enter the meter to convert:'))
 
-print("School: PHINMA Araullo University")
-print("College: CITE")
+millimeter = meter * 1000
+centimeter = meter * 100
+decimeter = meter * 10
+kilometer = meter / 1000
+inch = meter * 39.37
+foot = meter * 3.28
+mile = meter / 1609.344
 
-
-num1 =  5
-num2 = 8
-
-sum =  num1 + num2
-print("SUM = {0}".format(sum))
-
-difference =  num1 - num2
-print("difference = {0}".format(difference))
-
-product =  num1 * num2
-print("product = {0}".format(product))
-
-quotient =  num1 / num2
-print("quotient = {0}".format(quotient))
+print('Meter to millimeter :', millimeter)
+print('Meter to centimeter :', centimeter)
+print('Meter to decimeter :', decimeter)
+print('Meter to kilometer :', kilometer)
+print('Meter to inch :', inch)
+print('Meter to foot :', foot)
+print('Meter to mile :', mile)
